2853c91d52ca6597faa882036be8b6469fe42fa5
Yoothakarn_Mobipax <yooth@mobipax.biz>
2019-06-24 12:07:32 +0700

    Add Unit Test Project

---
+++ TestProject/XUnitTestProject/CustomerControllerTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using AutoMapper;
+using TestProject.Controllers;
+using TestProject.Domain.Services;
+using TestProject.Services;
+using TestProject.Mapping;
+using TestProject.Enquery;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace XUnitTestProject
+{
+    public class CustomerControllerTest
+    {
+        private readonly ICustomerService _customerService;
+        private readonly IMapper _mapper;
+
+        public CustomerControllerTest(ICustomerService customerService, IMapper mapper)
+        {
+            _customerService = customerService;
+            _mapper = mapper;
+        }
+
+        [Fact]
+        public void Get_WhenCalled_WithNoInquery()
+        {
+            CustomersController _controller = new CustomersController(_customerService, _mapper);
+
+            // Act
+            var result = _controller.ListAsync(null);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+
+        }
+
+        [Fact]
+        public void Get_WhenCalled_WithInqueryCustomerId()
+        {
+            CustomerEnquery en = new CustomerEnquery();
+            en.CustomerId = 100001;
+
+            CustomersController _controller = new CustomersController(_customerService, _mapper);
+
+            // Act
+            var result = _controller.ListAsync(en);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+
+        }
+
+
+        [Fact]
+        public void Get_WhenCalled_WithInqueryEmail()
+        {
+            CustomerEnquery en = new CustomerEnquery();
+            en.Email = "FirstName1@gmail.com";
+
+            CustomersController _controller = new CustomersController(_customerService, _mapper);
+
+            // Act
+            var result = _controller.ListAsync(en);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+
+        }
+    }
+}
---
+++ TestProject/XUnitTestProject/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.DependencyInjection;
+using TestProject.Controllers.Config;
+using TestProject.Domain.Repositories;
+using TestProject.Domain.Services;
+using TestProject.Persistence.Contexts;
+using TestProject.Persistence.Repositories;
+using TestProject.Services;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+using AutoMapper;
+
+[assembly: TestFramework("XUnitTestProject.Startup", "XUnitTestProject")]
+namespace XUnitTestProject
+{
+    public class Startup : DependencyInjectionTestFramework
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IMessageSink messageSink) : base(messageSink)
+        {
+
+            var builder = new ConfigurationBuilder().AddJsonFile("appSettings.json");
+            Configuration = builder.Build();
+        }
+
+        protected override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+
+            services.AddDbContext<AppDbContext>
+          (options => options.UseSqlServer(Configuration.GetConnectionString("TestDatabase")));
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<ICustomerService, CustomerService>();
+
+            services.AddAutoMapper();
+        }
+    }
+}
